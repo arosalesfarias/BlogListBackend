d698bae73b55bc06b36967d1ffaebf897e8fa01e
Ariel Rosales <arosalesfarias@gmail.com>
2024-10-14 00:22:36 -0300

    Part 4.2, Blog list step 2. Create App file and finish step 2.

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
 const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
