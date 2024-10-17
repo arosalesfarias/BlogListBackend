8a8146151bd797121e0c555ea9bb418807b911a3
Ariel Rosales <arosalesfarias@gmail.com>
2024-10-17 01:25:18 -0300

    Part 4.4, auxiliary functions and unit tests step 2. Total Likes function

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -8,3 +8,103 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
 })
+
+describe('Total Likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const bigList = [
+        {
+            _id: '5a422aa71b54a676234d17f0',
+            title: 'Level 0',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f1',
+            title: 'Level 1',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 8,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f2',
+            title: 'Level 2',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 65,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f3',
+            title: 'Level 3',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 12,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f4',
+            title: 'Level 4',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 2,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f5',
+            title: 'Level 5',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 51,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f6',
+            title: 'Level 6',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 33,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f7',
+            title: 'Level 7',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 12,
+            __v: 0
+        },{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Level 8',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 21,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f9',
+            title: 'Level 9',
+            author: 'Don Satur',
+            url: 'https://donsatur.io',
+            likes: 100,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        assert.strictEqual(listHelper.totalLikes(listWithOneBlog), listWithOneBlog[0].likes)
+    })
+    test('of empty list is zero', () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0)
+    })
+    test('of a bigger list is calculated right', () => {
+        assert.strictEqual(listHelper.totalLikes(bigList), 309)
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,17 @@
+const Blog = require('../models/blog')
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (arr) => {
+    const sumLikes = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return arr.reduce(sumLikes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
